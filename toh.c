f37dfb8a6337933be13873dd4591af3e7cecb1df
chetan verma <58814689+Chetan-verma@users.noreply.github.com>
2021-08-15 18:47:35 +0530

    Initialize
    This is C code for "Tower of Hanoi"
    The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower and sometimes pluralized as Towers, or simply pyramid puzzle ) is a mathematical game or puzzle. It consists of three rods and a number of disks of different diameters, which can slide onto any rod. The puzzle starts with the disks stacked on one rod in order of decreasing size, the smallest at the top, thus approximating a conical shape.

---
+++ toh.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void Move(int Disc, int tower1, int tower3, int temp_tower);//to move disc from one tower to another tower
+int main(){
+    int Disc;
+    printf("Enter the number of Disc in tower 1: ");
+    scanf("%d",&Disc);
+    if (Disc>15)
+    {
+        printf("It will take lot of time in your so please don't try beyond 15: ");
+        return 0;
+    }
+    Move(Disc, 1, 3, 2);
+    return 0;
+}
+void Move(int Disc, int tower1, int tower3, int temp_tower){
+    if(Disc!=0){
+    int y;//it  is varialbe wich will update disc every time. 
+    y=Disc-1;
+    Move(y, tower1, temp_tower, tower3);
+    printf("Move disk Disc %d from tower %d to tower %d.\n",Disc, tower1, tower3);
+    Move(y, temp_tower, tower3, tower1);
+    }
+}
