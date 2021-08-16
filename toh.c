a0e16a1c7a87d9882f812f81a693180c1b187a6e
chetan verma <58814689+Chetan-verma@users.noreply.github.com>
2021-08-16 11:11:55 +0530

    Update toh.c

--- toh.c
+++ toh.c
@@ -6,7 +6,7 @@ int main(){
     scanf("%d",&Disc);
     if (Disc>15)
     {
-        printf("It will take lot of time in your so please don't try more thanS 15: ");
+        printf("It will take lot of time in your system so please don't try more than 15: ");
         return 0;
     }
     Move(Disc, 1, 3, 2);
@@ -14,7 +14,7 @@ int main(){
 }
 void Move(int Disc, int tower1, int tower3, int temp_tower){
     if(Disc!=0){
-    int y;//it  is varialbe wich will update disc every time. 
+    int y;//it  is varialbe which will update disc every time while calling the function by itself.
     y=Disc-1;
     Move(y, tower1, temp_tower, tower3);
     printf("Move disk Disc %d from tower %d to tower %d.\n",Disc, tower1, tower3);
